fecfca4cf2614dbf95f09952904bea562ed62561
Miles Worthington <meworthington1@gmail.com>
2021-12-28 23:43:19 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+let satMsg;
+let monMsg;
+
+function saturdayFun(activity) {
+    activity = activity ? satMsg = `This Saturday, I want to ${activity}!` : satMsg = `This Saturday, I want to roller-skate!`;
+    return satMsg;
+}
+
+const mondayWork = function (activity) {
+    activity = activity ? monMsg = `This Monday, I will ${activity}.` : monMsg = "This Monday, I will go to the office.";
+    return monMsg;
+}
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
